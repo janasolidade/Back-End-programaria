7cc228e4c889f97087c1f5c120778d462fbab57b
janasolidade <janaina.solidade@gmail.com>
2023-09-29 21:06:21 -0300

    adiciona a primeira versão do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+const mulheres = [
+    {
+        nome: "Janaina Solidade",
+        imagem: "https://media.licdn.com/dms/image/D4D03AQFYJ8V3pmGZTw/profile-displayphoto-shrink_800_800/0/1678996115001?e=1700092800&v=beta&t=fFwVTEISBQAQ4kH21lsmpROMPgO8OHLHSu_AX6y5N1E",
+        minibio: "Estudante de tecnologia"
+    } ,
+    {
+        nome: "Simaria Conceição",
+        imagem: "",
+        minibio: "Desenvolvedora e professora"
+    },
+    {
+        nome: "Iana Chan",
+        imagem: "",
+        minibio: "Fundadora do Programaria"
+    }  
+]
+
+function mostraMulheres(request, response){
+    response.json(mulheres)
+}
+
+function mostraPorta (){
+    console.log("Servidor criado e rodando na porta ", porta) 
+}
+
+app.use(router.get("/mulheres", mostraMulheres))
+app.listen(porta, mostraPorta)
